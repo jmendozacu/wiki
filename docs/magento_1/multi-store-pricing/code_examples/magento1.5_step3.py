465899a001930ef721412332212d8c559985324b
Trung Nguyen <trungbssgroup@gmail.com>
2016-06-15 10:10:36 +0700

    change installation guide multi store princing

--- docs/magento_1/multi-store-pricing/code_examples/magento1.5_step3.py
+++ docs/magento_1/multi-store-pricing/code_examples/magento1.5_step3.py
@@ -1,26 +1,28 @@
 //add modify code get store price
-            if(Mage::helper('multistoreviewpricing')->isScopePrice() == 1) {
-        	 	$select = $this->getSelect();
-            	$tableName = Mage::getSingleton('core/resource')->getTableName('catalog_product_entity_decimal');
-            	$store_id = $this->getStoreId();
+if(Mage::helper('multistoreviewpricing')->isScopePrice() == 1) {
+    $select = $this->getSelect();
+    $tableName = Mage::getSingleton('core/resource')->getTableName('catalog_product_entity_decimal');
+    $store_id = $this->getStoreId();
  
-            	$minimalExpr = $connection->getCheckSql('bss_price_table_1.value IS NOT NULL', 'bss_price_table_1.value', 'price_index.price');
+    $minimalExpr = $connection->getCheckSql('bss_price_table_1.value IS NOT NULL', 'bss_price_table_1.value', 'price_index.price');
  
-                $select->joinLeft(
-                    array('bss_price_table_1' => $tableName),
-                    "e.entity_id = bss_price_table_1.entity_id
-                	AND bss_price_table_1.attribute_id = (SELECT attribute_id FROM " . Mage::getSingleton('core/resource')->getTableName('eav/attribute') . " WHERE attribute_code = 'price' AND backend_model != '' LIMIT 1) 
-           	     AND bss_price_table_1.store_id = ".$store_id,
-                    array('price' => $minimalExpr,  'final_price' => 'bss_price_table_1.value','minimal_price'=> 'bss_price_table_1.value')
-                	);
+    $select->joinLeft(
+        array('bss_price_table_1' => $tableName),
+        "e.entity_id = bss_price_table_1.entity_id
+        AND bss_price_table_1.attribute_id = (SELECT attribute_id FROM " . Mage::getSingleton('core/resource')->getTableName('eav/attribute') . 
+				" WHERE attribute_code = 'price' AND backend_model != '' LIMIT 1) 
+        AND bss_price_table_1.store_id = ".$store_id,
+        array('price' => $minimalExpr,  'final_price' => 'bss_price_table_1.value','minimal_price'=> 'bss_price_table_1.value')
+        );
  
-                $select->joinLeft(
- 	               array('bss_price_table_2' => $tableName),
-                    "e.entity_id = bss_price_table_2.entity_id
-                	AND bss_price_table_2.attribute_id = (SELECT attribute_id FROM " . Mage::getSingleton('core/resource')->getTableName('eav/attribute') . " WHERE attribute_code = 'special_price' AND backend_model != '' LIMIT 1) 
-                	AND bss_price_table_2.store_id = ".$store_id."
-                	AND bss_price_table_2.value < bss_price_table_1.value",
-	                array('final_price' => 'bss_price_table_2.value','minimal_price'=> 'bss_price_table_2.value')
-                	);
-        	}
-        	//end
+    $select->joinLeft(
+ 	    array('bss_price_table_2' => $tableName),
+        "e.entity_id = bss_price_table_2.entity_id
+        AND bss_price_table_2.attribute_id = (SELECT attribute_id FROM " . Mage::getSingleton('core/resource')->getTableName('eav/attribute') . 
+				" WHERE attribute_code = 'special_price' AND backend_model != '' LIMIT 1) 
+        AND bss_price_table_2.store_id = ".$store_id."
+        AND bss_price_table_2.value < bss_price_table_1.value",
+	    array('final_price' => 'bss_price_table_2.value','minimal_price'=> 'bss_price_table_2.value')
+       );
+}
+//end
